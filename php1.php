e2905a836a93eafd53db95503659ca91a7cd5ed4
Jar-ed17 <164950728+Jar-ed17@users.noreply.github.com>
2024-11-06 15:20:44 -0500

    Create php1.php

---
+++ php1.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo"Hola, Este es mi primer script de PHP <BR>";
+
+// Esto es una linea de comenterio
+
+#Esto tambien es una linea de comentario
+
+/*Esto es 
+un bloqye de mas de una linea 
+de comentario*/
+
+$vartext="Hola! Ahora estoy usando variables para guardar texto<BR>";
+echo $vartext;
+$varnum1=5;
+$varnum2= 6;
+$res=$varnum1+$varnum2;
+echo"El resultado de la suma de $varnum1 y $varnum2 es igual a $res";
+?>
+</body>
+</html>
