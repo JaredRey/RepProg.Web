ec4e73f67b55d0292b3ceadcea7d042a51cbc73f
Jar-ed17 <L20530273@cancun.tecnm.mx>
2024-11-09 21:44:13 -0500

    php activities

---
+++ actividad2php.php
@@ -0,0 +1,16 @@
+<?php
+$a=10;
+
+function variable(){
+    $b= 15;
+    echo"Prueba de echo<br>";
+    echo"Variable A es $a <br>";
+    echo"Variable B es $b <br><BR>";
+}
+
+variable();
+    echo "Prueba de impresion fera de funtion";
+    echo"Variable A es $a<br>";
+    echo "Variable B es $b<br>";
+
+?>
